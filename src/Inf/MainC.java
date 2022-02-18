a0dc7913f73f395ec665782c802e4fa18a94577d
jhm <moomjh94@naver.com>
2022-02-18 17:00:02 +0900

    6-5

--- src/Inf/MainC.java
+++ src/Inf/MainC.java
@@ -2,12 +2,18 @@ package Inf;
 
 public class MainC {
     public static void main(String[] args) {
+        long start = System.currentTimeMillis(); //시작하는 시점 계산
+        
         SolveProblem solve = new Solve();//2단원
-                     solve = new Solve3();//3단원
-                     solve = new Solve4();//4단원
-                     solve = new Solve5();//5단원
-                     solve = new Solve6();//6단원
-                     solve.Problem();
+        solve = new Solve3();//3단원
+        solve = new Solve4();//4단원
+        solve = new Solve5();//5단원
+        solve = new Solve6();//6단원
+        solve.Problem();
+
+        long end = System.currentTimeMillis(); //프로그램이 끝나는 시점 계산
+        System.out.println( "실행 시간 : " + ( end - start )/1000.0 +"초"); //실행 시간 계산 및 출력
+
 
 
     }
--- src/Inf/Solve6.java
+++ src/Inf/Solve6.java
@@ -1,15 +1,18 @@
 package Inf;
 
 import java.util.Arrays;
+import java.util.Stack;
+import java.util.TreeSet;
 
 public class Solve6 implements SolveProblem{
 
     @Override
     public void Problem() {
        //P6_1();//선택정렬
-        //P6_2();//버블정렬
-        P6_3();//삽입정렬
-       // P6_4();
+       //P6_2();//버블정렬
+       //P6_3();//삽입정렬
+       //P6_4();
+        P6_5();
 
     }
 
@@ -183,4 +186,174 @@ public class Solve6 implements SolveProblem{
         }
 
     }
+
+    public void P6_4(){
+
+        /**
+         * 4. Least Recently Used
+         * 설명
+         *
+         * 캐시메모리는 CPU와 주기억장치(DRAM) 사이의 고속의 임시 메모리로서 CPU가 처리할 작업을 저장해 놓았다가
+         *
+         * 필요할 바로 사용해서 처리속도를 높이는 장치이다. 워낙 비싸고 용량이 작아 효율적으로 사용해야 한다.
+         *
+         * 철수의 컴퓨터는 캐시메모리 사용 규칙이 LRU 알고리즘을 따른다.
+         *
+         * LRU 알고리즘은 Least Recently Used 의 약자로 직역하자면 가장 최근에 사용되지 않은 것 정도의 의미를 가지고 있습니다.
+         *
+         * 캐시에서 작업을 제거할 때 가장 오랫동안 사용하지 않은 것을 제거하겠다는 알고리즘입니다.
+         *
+         * Image1.jpg
+         *
+         * 캐시의 크기가 주어지고, 캐시가 비어있는 상태에서 N개의 작업을 CPU가 차례로 처리한다면 N개의 작업을 처리한 후
+         *
+         * 캐시메모리의 상태를 가장 최근 사용된 작업부터 차례대로 출력하는 프로그램을 작성하세요.
+         *
+         *
+         * 입력
+         * 첫 번째 줄에 캐시의 크기인 S(3<=S<=10)와 작업의 개수 N(5<=N<=1,000)이 입력된다.
+         *
+         * 두 번째 줄에 N개의 작업번호가 처리순으로 주어진다. 작업번호는 1 ~100 이다.
+         *
+         *
+         * 출력
+         * 마지막 작업 후 캐시메모리의 상태를 가장 최근 사용된 작업부터 차례로 출력합니다.
+         *
+         *
+         * 예시 입력 1
+         *
+         * 5 9
+         * 1 2 3 2 6 2 3 5 7
+         * 예시 출력 1
+         *
+         * 7 5 3 2 6
+         */
+
+        /**
+         *  캐시 미스 : 캐시에 주어진 작업이 없을 때
+         *  캐시 히트 : 캐시에 주어진 작업이 있을 때
+         *
+         *  1. 캐시 메모리 순회
+         *  2. 숫자 찾으면 캐시 히트
+         *  3. 캐시 히트 시 찾은 인덱스 부터 c[i]=c[i-1] 형식으로 복사
+         *  4. 캐시 미스 시 맨 끝부터 c[i]=c[i-1] 형식으로 복사
+         *  5. 현재 작업은 c[0]
+         */
+
+        int S = 5;
+        int N = 9;
+        int works[] = {1, 2 ,3, 2, 6, 2, 3, 5 ,7};
+        int cache[] = new int [S];
+
+        for (int i=0; i<N ;i++){
+            int pos = -1;
+            for( int j=0 ;j<S ; j++){
+                //캐시 히트, 미스 여부 탐색
+                if(cache[j]==works[i]){//히트
+                    pos=1;
+                    System.out.println("히트" + cache[j]);
+                    for(int k =j ; k>=1 ;k--){
+                        cache[k] = cache[k-1];
+                    }
+                    cache[0] = works[i];
+                 break;
+                }
+            }
+            if(pos==-1) {
+                //캐시 미스
+                for (int k = S - 1; k > 0; k--) {
+                    cache[k] = cache[k - 1];
+                }
+                cache[0] = works[i];
+                System.out.println("미스" + cache[0]);
+            }
+        }
+
+        System.out.println(cache);
+
+
+
+    }
+
+    public void P6_5(){
+        /**
+         * 5. 중복 확인
+         * 설명
+         *
+         * 현수네 반에는 N명의 학생들이 있습니다.
+         *
+         * 선생님은 반 학생들에게 1부터 10,000,000까지의 자연수 중에서 각자가 좋아하는 숫자 하나 적어 내라고 했습니다.
+         *
+         * 만약 N명의 학생들이 적어낸 숫자 중 중복된 숫자가 존재하면 D(duplication)를 출력하고,
+         *
+         * N명이 모두 각자 다른 숫자를 적어냈다면 U(unique)를 출력하는 프로그램을 작성하세요.
+         *
+         *
+         * 입력
+         * 첫 번째 줄에 자연수 N(5<=N<=100,000)이 주어진다.
+         *
+         * 두 번째 줄에 학생들이 적어 낸 N개의 자연수가 입력된다.
+         *
+         *
+         * 출력
+         * 첫 번째 줄에 D 또는 U를 출력한다.
+         *
+         *
+         * 예시 입력 1
+         *
+         * 8
+         * 20 25 52 30 39 33 43 33
+         * 예시 출력 1
+         *
+         * D
+         */
+
+        int N = 8;
+        int arr[] ={20 ,25 ,52 ,30 ,39 ,33 ,43 ,33};
+
+        String answer = "U";
+
+       /* for(int i =0 ;i<N ;i++){
+            for(int j=i+1;j<N ;j++){
+                if(arr[i]==arr[j]){
+                    answer="D";
+                    break;
+                }
+            }
+        }*/
+        Stack<Integer> stk = new Stack<>();
+        for(int a :arr){
+            if(!stk.isEmpty() && stk.contains(a)){
+                answer="D";
+                break;
+            }else{
+                stk.push(a);
+            }
+        }
+
+        System.out.println(answer);
+
+
+    }
+
+    public void P6_6(){
+
+    }
+
+    public void P6_7(){
+
+    }
+
+    public void P6_8(){
+
+    }
+
+    public void P6_9(){
+
+    }
+
+    public void P6_10(){
+
+    }
+
 }
